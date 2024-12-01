6dcd4d16d82f5f39fd363d22b24193e39cbde388
PlusBoy <no1plus88@gmail.com>
2024-12-01 23:40:50 +0800

    反馈接口 添加ip以及频控

--- icutool-toolBackend/src/main/java/cn/icutool/controller/UserFeedbackController.java
+++ icutool-toolBackend/src/main/java/cn/icutool/controller/UserFeedbackController.java
@@ -1,5 +1,8 @@
 package cn.icutool.controller;
 
+import cn.icutool.common.RequestHolder;
+import cn.icutool.common.annotation.FrequencyControl;
+import cn.icutool.domain.dto.RequestInfo;
 import cn.icutool.domain.entity.UserFeedback;
 import cn.icutool.domain.vo.request.UserFeedbackReq;
 import cn.icutool.domain.vo.request.UserRegisterReq;
@@ -31,9 +34,14 @@ public class UserFeedbackController {
 
     @PostMapping("/add")
     @ApiOperation("添加反馈")
+    @FrequencyControl(time = 5, count = 3, target = FrequencyControl.Target.IP)
+    @FrequencyControl(time = 30, count = 5, target = FrequencyControl.Target.IP)
+    @FrequencyControl(time = 300, count = 10, target = FrequencyControl.Target.IP)
     public AjaxResult add(@Valid @RequestBody UserFeedbackReq userFeedbackReq){
         UserFeedback userFeedback = new UserFeedback();
         BeanUtils.copyProperties(userFeedbackReq, userFeedback);
+        RequestInfo requestInfo = RequestHolder.get();
+        userFeedback.setIp(requestInfo.getIp());
         userFeedbackService.add(userFeedback);
         return AjaxResult.success("添加反馈内容成功,等待管理员查看~");
     }
