420e6669552a57621a64a49fd0b7e26607cb6b62
icutool <no1plus88@gmail.com>
2024-12-03 10:57:40 +0800

    添加网关相关信息

---
+++ icutool-gateway/src/main/java/cn/icutool/icutoolgateway/IcutoolGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.icutool.icutoolgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcutoolGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcutoolGatewayApplication.class, args);
+    }
+
+}
