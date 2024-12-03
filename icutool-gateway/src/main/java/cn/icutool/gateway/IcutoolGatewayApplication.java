c5f45f17df538976f6a3abcac21b0bf02de20f6c
PlusBoy <no1plus88@gmail.com>
2024-12-03 20:17:46 +0800

    网关添加跨域支持

--- icutool-gateway/src/main/java/cn/icutool/gateway/IcutoolGatewayApplication.java
+++ icutool-gateway/src/main/java/cn/icutool/gateway/IcutoolGatewayApplication.java
@@ -1,4 +1,4 @@
-package cn.icutool.icutoolgateway;
+package cn.icutool.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ icutool-gateway/src/main/java/cn/icutool/gateway/config/GlobalCorsConfig.java
@@ -0,0 +1,29 @@
+package cn.icutool.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * 全局跨域配置
+ * 注意：前端从网关进行调用时需要配置
+ * Created by macro on 2019/7/27.
+ */
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");  // 使用具体的源地址或 * 来允许所有域名
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+
+}
