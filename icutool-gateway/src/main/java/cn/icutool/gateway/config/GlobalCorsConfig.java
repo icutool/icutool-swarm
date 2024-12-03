9e83a4987b743fd48961f7454cf7533bc243845c
PlusBoy <no1plus88@gmail.com>
2024-12-03 20:20:05 +0800

    网关添加跨域支持

--- icutool-gateway/src/main/java/cn/icutool/gateway/config/GlobalCorsConfig.java
+++ icutool-gateway/src/main/java/cn/icutool/gateway/config/GlobalCorsConfig.java
@@ -8,8 +8,6 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 /**
  * 全局跨域配置
- * 注意：前端从网关进行调用时需要配置
- * Created by macro on 2019/7/27.
  */
 @Configuration
 public class GlobalCorsConfig {
