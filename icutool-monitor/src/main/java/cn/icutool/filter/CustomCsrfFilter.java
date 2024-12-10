63134880a42d2c2884c56735e1ba7b91abc19b8d
icutool <no1plus88@gmail.com>
2024-12-10 18:51:29 +0800

    test monitor

---
+++ icutool-monitor/src/main/java/cn/icutool/filter/CustomCsrfFilter.java
@@ -0,0 +1,40 @@
+package cn.icutool.filter;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @auther macrozheng
+ * @description 自定义CSRF过滤器
+ * @date 2024/1/16
+ * @github https://github.com/macrozheng
+ */
+public class CustomCsrfFilter extends OncePerRequestFilter {
+
+    public static final String CSRF_COOKIE_NAME = "XSRF-TOKEN";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        if (csrf != null) {
+            Cookie cookie = WebUtils.getCookie(request, CSRF_COOKIE_NAME);
+            String token = csrf.getToken();
+            if (cookie == null || token != null && !token.equals(cookie.getValue())) {
+                cookie = new Cookie(CSRF_COOKIE_NAME, token);
+                cookie.setPath("/");
+                response.addCookie(cookie);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
